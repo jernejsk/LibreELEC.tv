11b147805a58d8fc4679383d26855e34f2756f13
Jernej Skrabec <jernej.skrabec@siol.net>
2018-12-08 09:47:50 +0100

    sunxi-mali: Allow aarch64 build

--- packages/graphics/sunxi-mali/package.mk
+++ packages/graphics/sunxi-mali/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="sunxi-mali"
 PKG_VERSION="418f55585e76f375792dbebb3e97532f0c1c556d"
 PKG_SHA256="b5e7e8f9f2886ed0b273f72ea16ae4868711726fe33e3d80ef24e86269c90fd2"
-PKG_ARCH="arm"
+PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/bootlin/mali-blobs"
 PKG_URL="https://github.com/bootlin/mali-blobs/archive/$PKG_VERSION.tar.gz"
@@ -13,6 +13,15 @@ PKG_DEPENDS_TARGET="toolchain libdrm wayland"
 PKG_LONGDESC="Sunxi Mali-400 support libraries"
 PKG_TOOLCHAIN="manual"
 
+if [ "$TARGET_ARCH" = "arm" ]; then
+  PKG_MALI_ARCH="arm"
+elif [ "$TARGET_ARCH" = "aarch64" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libffi"
+  PKG_MALI_ARCH="arm64"
+fi
+
+PKG_MALI_FILE="r6p2/$PKG_MALI_ARCH/wayland/libMali.so"
+
 makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/include/
     cp -av include/wayland/* $SYSROOT_PREFIX/usr/include
@@ -20,13 +29,11 @@ makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/lib/pkgconfig
     cp -PRv $PKG_DIR/pkgconfig/*.pc $SYSROOT_PREFIX/usr/lib/pkgconfig
 
-  MALI="r6p2/arm/wayland/libMali.so"
-
   mkdir -p $SYSROOT_PREFIX/usr/lib/
-    cp -v $MALI $SYSROOT_PREFIX/usr/lib
+    cp -v $PKG_MALI_FILE $SYSROOT_PREFIX/usr/lib
 
   mkdir -p $INSTALL/usr/lib
-    cp -v $MALI $INSTALL/usr/lib
+    cp -v $PKG_MALI_FILE $INSTALL/usr/lib
 
     for lib in libEGL.so \
                libEGL.so.1 \
